7991f717525673866b209318b488f983e78a65b3
Thibault Bondetti <thibault.bondetti@cgi.com>
2026-07-30 10:08:11 +0200

    [feat] Corrections scopes

--- test-resource-server-infrastructure/src/main/java/io/github/tbondetti/testresourceserver/infrastructure/constants/Scopes.java
+++ test-resource-server-infrastructure/src/main/java/io/github/tbondetti/testresourceserver/infrastructure/constants/Scopes.java
@@ -4,8 +4,8 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Scopes {
-    public static final String PRODUIT_READ = "SCOPE_trs:product-api.read";
-    public static final String PRODUIT_WRITE = "SCOPE_trs:product-api.write";
+    public static final String PRODUIT_READ = "SCOPE_trs:produit-api.read";
+    public static final String PRODUIT_WRITE = "SCOPE_trs:produit-api.write";
     public static final String COMMANDE_READ = "SCOPE_trs:commande-api.read";
     public static final String COMMANDE_WRITE = "SCOPE_trs:commande-api.write";
 
