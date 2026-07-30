7b8d80ea4cc0bb74298bd7b9ec738f019cdce6e6
Thibault Bondetti <thibault.bondetti@cgi.com>
2026-07-30 11:51:53 +0200

    [doc] Mise à jour documentation

--- test-resource-server-infrastructure/src/test/java/io/github/tbondetti/testresourceserver/infrastructure/api/v1/error/ApiExceptionHandlerTest.java
+++ test-resource-server-infrastructure/src/test/java/io/github/tbondetti/testresourceserver/infrastructure/api/v1/error/ApiExceptionHandlerTest.java
@@ -2,6 +2,7 @@ package io.github.tbondetti.testresourceserver.infrastructure.api.v1.error;
 
 import io.github.tbondetti.testresourceserver.core.exception.ResourceServerErrorCode;
 import io.github.tbondetti.testresourceserver.core.exception.ResourceServerFunctionalException;
+import io.github.tbondetti.testresourceserver.core.exception.ResourceServerNotFoundException;
 import io.github.tbondetti.testresourceserver.core.exception.ResourceServerTechnicalException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,12 +14,32 @@ import static io.github.tbondetti.testresourceserver.core.exception.ResourceServ
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ExtendWith(MockitoExtension.class)
 class ApiExceptionHandlerTest {
 
     @InjectMocks
     private ApiExceptionHandler subject;
+
+    @Test
+    void handleNotFoundExceptionOk() {
+        final ResourceServerErrorCode code = PRODUIT_NOM_REQUIRED;
+        final String message = "Produit introuvable";
+
+        final ResourceServerNotFoundException exception = new ResourceServerNotFoundException(code, message);
+
+        final ResponseEntity<ApiErrorResponse> actual = this.subject.handleNotFoundException(exception);
+
+        assertEquals(NOT_FOUND.value(), actual.getStatusCode().value());
+
+        final ApiErrorResponse body = ApiErrorResponse.builder()
+                .code(code)
+                .description(message)
+                .build();
+
+        assertEquals(body, actual.getBody());
+    }
 
     @Test
     void handleAuthServerExceptionOk() {
